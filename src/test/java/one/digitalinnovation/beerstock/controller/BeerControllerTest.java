ac142b7b3ad3999f5f6e1842e5195bced140f3d6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-13 18:42:23 -0300

    implements test whenGETListWithBeersIsCalledThenOkStatusIsReturned

--- src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -17,6 +17,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import java.util.Collections;
+
 import static one.digitalinnovation.beerstock.utils.JsonConvertionUtils.asJsonString;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
@@ -101,6 +103,20 @@ public class BeerControllerTest {
     mockMvc.perform(MockMvcRequestBuilders.get(BEER_API_URL_PATH + "/" + beerDTO.getName())
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void whenGETListWithBeersIsCalledThenOkStatusIsReturned() throws Exception {
+    BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+    when(beerService.listAll()).thenReturn(Collections.singletonList(beerDTO));
+
+    mockMvc.perform(MockMvcRequestBuilders.get(BEER_API_URL_PATH)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].name", is(beerDTO.getName())))
+        .andExpect(jsonPath("$[0].brand", is(beerDTO.getBrand())))
+        .andExpect(jsonPath("$[0].type", is(beerDTO.getType().toString())));
   }
 
 }
