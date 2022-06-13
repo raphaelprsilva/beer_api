9b7b60fa70f1a4be9eac6a8b1769a48eccf9f350
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-13 19:01:57 -0300

    implements test whenIncrementIsGreatherThanMaxThenThrowException

--- src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
+++ src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
@@ -140,4 +140,16 @@ public class BeerServiceTest {
     assertThat(expectedQuantityAfterIncrement, equalTo(incrementedBeerDTO.getQuantity()));
     assertThat(expectedQuantityAfterIncrement, lessThan(expectedBeerDTO.getMax()));
   }
+
+  @Test
+  void whenIncrementIsGreatherThanMaxThenThrowException() {
+    BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+    Beer expectedBeer = beerMapper.toModel(expectedBeerDTO);
+
+    when(beerRepository.findById(expectedBeerDTO.getId())).thenReturn(Optional.of(expectedBeer));
+
+    int quantityToIncrement = 80;
+    assertThrows(BeerStockExceededException.class, () -> beerService.increment(expectedBeerDTO.getId(), quantityToIncrement));
+  }
+
 }
