59d38343fffd7db4086aacc648f75939847a33a5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-13 18:34:32 -0300

    implements test whenGETIsCalledWithoutRegisteredNameThenNotFoundStatusIsReturned

--- src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.beerstock.controller;
 
 import one.digitalinnovation.beerstock.builder.BeerDTOBuilder;
 import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.exception.BeerNotFoundException;
 import one.digitalinnovation.beerstock.service.BeerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
@@ -71,6 +73,17 @@ public class BeerControllerTest {
         .contentType(MediaType.APPLICATION_JSON)
         .content(asJsonString(beerDTO)))
         .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void whenGETIsCalledWithoutRegisteredNameThenNotFoundStatusIsReturned() throws Exception {
+    BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+    when(beerService.findByName(beerDTO.getName())).thenThrow(BeerNotFoundException.class);
+
+    mockMvc.perform(MockMvcRequestBuilders.get(BEER_API_URL_PATH + "/" + beerDTO.getName())
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
   }
 
 }
