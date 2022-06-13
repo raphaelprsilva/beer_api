9aab7c554060159c73c5c62d73d1cef414cdf024
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-13 19:07:59 -0300

    implements test whenPATCHIsCalledToIncrementDiscountThenOKstatusIsReturned

--- src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.beerstock.controller;
 
 import one.digitalinnovation.beerstock.builder.BeerDTOBuilder;
 import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.dto.QuantityDTO;
 import one.digitalinnovation.beerstock.exception.BeerNotFoundException;
 import one.digitalinnovation.beerstock.service.BeerService;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +23,7 @@ import java.util.Collections;
 import static one.digitalinnovation.beerstock.utils.JsonConvertionUtils.asJsonString;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -150,4 +152,23 @@ public class BeerControllerTest {
         .andExpect(status().isNotFound());
   }
 
+  @Test
+  void whenPATCHIsCalledToIncrementDiscountThenOKstatusIsReturned() throws Exception {
+    QuantityDTO quantityDTO = QuantityDTO.builder()
+        .quantity(10)
+        .build();
+
+    BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+    beerDTO.setQuantity(beerDTO.getQuantity() + quantityDTO.getQuantity());
+
+    when(beerService.increment(VALID_BEER_ID, quantityDTO.getQuantity())).thenReturn(beerDTO);
+
+    mockMvc.perform(MockMvcRequestBuilders.patch(BEER_API_URL_PATH + "/" + VALID_BEER_ID + BEER_API_SUBPATH_INCREMENT_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(quantityDTO))).andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+        .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+        .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())))
+        .andExpect(jsonPath("$.quantity", is(beerDTO.getQuantity())));
+  }
 }
